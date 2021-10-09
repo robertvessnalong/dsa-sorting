a2cbcb136d78a451a496cb20ef22cc4ce10ed236
Robert Long <pongrobertlong@gmail.com>
2021-10-09 01:15:11 -0400

    Finish Selection and Insertion

--- selection.js
+++ selection.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  const solver = (arr, idxOne, idxTwo) => {
+    return ([arr[idxOne], arr[idxTwo]] = [arr[idxTwo], arr[idxOne]]);
+  };
+  for (let i = 0; i < arr.length; i++) {
+    let low = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[low] > arr[j]) {
+        low = j;
+      }
+    }
+    if (i !== low) {
+      solver(arr, i, low);
+    }
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
